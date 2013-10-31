524ab99eda117f96f14e4e912234235d98d2395c
abhishek17 <abhishek17aug1991@gmail.com>
2013-10-31 06:56:26 -0200

    Enemy(Very Basic)

---
+++ P3/Assets/PlayerSight.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSight: MonoBehaviour 
+{
+	//Vector3 v =new Vector3(0,0,1);
+	//private int start = 0;
+	private int end = 30;
+	private int temp = -30;
+	
+	
+	void  Update (){
+		
+		Vector3 v = new Vector3(transform.position.x ,transform.position.y+0.5f,transform.position.z);
+		//Vector3 v2 =new Vector3(transform.position.x,tranform.position.y+5,transform.position.z); 
+		Vector3 v3=new Vector3(temp,0,end);
+		
+		RaycastHit hit;
+		if(temp <end)
+		{
+			//Debug.DrawLine(transform.position, v, Color.red);
+			if (Physics.Raycast(v, v3.normalized, out hit, 1000))
+			{
+			
+				print ("1");
+				print (hit.collider.gameObject.name);
+				if (hit.collider.gameObject.name == "First Person Controller")
+				{
+					audio.Play();
+					print ("2");
+				}
+			}
+			temp++;
+		}
+		else
+		{
+			temp = -30;
+		}
+	}
+}
---
+++ P3/Assets/RayCast.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+
+
+	
+	var maxDist=100;
+	function  Update (){
+		var hit: RaycastHit;
+		if (Physics.Raycast(transform.position, transform.forward,hit, maxDist))
+		{
+			
+			Debug.Log(hit);
+			Debug.DrawLine (transform.position, hit.point, Color.cyan);
+		}
+		
+	}
