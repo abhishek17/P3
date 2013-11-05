fb6106631d2a084c78b35ca438056b11829b62f4
wannabeemetal <siddharth4u2003@yahoo.com>
2013-11-04 22:01:37 -0800

    Improved sight

--- P3/Assets/PlayerSight.cs
+++ P3/Assets/PlayerSight.cs
@@ -3,38 +3,78 @@ using System.Collections;
 
 public class PlayerSight: MonoBehaviour 
 {
-	//Vector3 v =new Vector3(0,0,1);
-	//private int start = 0;
+
 	private int end = 30;
 	private int temp = -30;
+	private int temp2 = 30;
+	Vector3 dir;
+	Vector3 dir2;
+	Vector3 dir3;
+	public int reach = 20;   //Increase this to increase reach of ray
+	public float lowness = 0;
+	Vector3 v;
+	Vector3 v2;
+	RaycastHit hit;
+	RaycastHit hit2;
+	RaycastHit hit3;
 	
+	void Awake()
+	{
+	v = new Vector3(transform.position.x ,transform.position.y+0.5f,transform.position.z);
+	v2 = new Vector3(transform.position.x ,transform.position.y,transform.position.z);
+	}
 	
 	void  Update (){
 		
-		Vector3 v = new Vector3(transform.position.x ,transform.position.y+0.5f,transform.position.z);
-		//Vector3 v2 =new Vector3(transform.position.x,tranform.position.y+5,transform.position.z); 
-		Vector3 v3=new Vector3(temp,0,end);
+		float theta = transform.eulerAngles.y + 90+temp;
+		float theta2 = transform.eulerAngles.y + 90-temp;
 		
-		RaycastHit hit;
+		theta = theta*3.14f/180f;
+		theta2 = theta2*3.14f/180f;
+
+		//print (transform.rotation.y);
+		dir=new Vector3((-1)*reach*Mathf.Cos(theta),0,reach*Mathf.Sin(theta));
+		dir2=new Vector3((-1)*reach*Mathf.Cos(theta2),0,reach*Mathf.Sin(theta2));
+		dir3=new Vector3((-1)*reach*Mathf.Cos(theta2),(-1)*lowness,reach*Mathf.Sin(theta2));
+
+		Debug.DrawRay(v,dir,Color.blue);
+		Debug.DrawRay(v2,dir2,Color.red);
+		Debug.DrawRay(v,dir3,Color.green);
+
 		if(temp <end)
 		{
-			//Debug.DrawLine(transform.position, v, Color.red);
-			if (Physics.Raycast(v, v3.normalized, out hit, 1000))
+
+			if (Physics.Raycast(v, dir.normalized, out hit, 1000))
 			{
-			
-				print ("1");
-				print (hit.collider.gameObject.name);
 				if (hit.collider.gameObject.name == "First Person Controller")
+					{
+						audio.Play();
+
+					}
+				}
+			if (Physics.Raycast(v2, dir2.normalized, out hit2, 1000))
+			{
+
+				if (hit2.collider.gameObject.name == "First Person Controller")
 				{
 					audio.Play();
-					print ("2");
+				}
+			}
+			if (Physics.Raycast(v, dir3.normalized, out hit3, 1000))
+			{
+				print ("green "+hit3.collider.gameObject.name);
+				if (hit3.collider.gameObject.name == "First Person Controller")
+				{
+					audio.Play();
 				}
 			}
 			temp++;
+			temp2--;
 		}
 		else
 		{
 			temp = -30;
+			temp2 = 30;
 		}
 	}
 }
