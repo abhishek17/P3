18d6731b6241d447722aa7fd3a14943fad5ceb3a
abhishek17 <abhishek17aug1991@gmail.com>
2013-11-07 11:15:35 -0700

    Improved crawl+ Hua Li`s sounds

--- P3/Assets/Crouch.js
+++ P3/Assets/Crouch.js
@@ -13,30 +13,34 @@ private var chMotor: CharacterMotor;
 private var ch: CharacterController;
 private var tr: Transform;
 private var height: float; // initial height
+private var radius: float;
 var crouching=false; 
  
 function Start(){
     chMotor = GetComponent(CharacterMotor);
     tr = transform;
     ch = GetComponent(CharacterController);
-	height = ch.height;
+ height = ch.height;
+ radius = ch.radius;
 }
  
 function Update(){
  
     var h = height;
+    var r = radius;
     var speed = walkSpeed;
     var temp:float=0;
     var voice=walkSound;
     var pitch=walkPitch;
     if (ch.isGrounded && Input.GetKey("left shift") || Input.GetKey("right shift")){
-    	voice=runSound;
+     voice=runSound;
         speed = runSpeed;
         pitch=runPitch;
     }
     if (Input.GetKey("v")){ // press v to crawl
-    	
+     
         h = 0.05 * height;
+        r=0.8*radius;
         speed = crawlSpeed; // slow down when crouching
         voice=crchSound;
         pitch=crchPitch;
@@ -47,7 +51,7 @@ function Update(){
     temp=-0.03;
     }
     if (Input.GetKey("c")){ // press C to crouch
-    	
+     
         h = 0.6 * height;
         speed = crchSpeed; // slow down when crouching
         voice=crchSound;
@@ -55,9 +59,10 @@ function Update(){
         //temp=0.03;
     }
     chMotor.movement.maxForwardSpeed = speed; // set max speed
-    var lastHeight = ch.height; // crouch/stand up smoothly 
-    ch.height = Mathf.Lerp(ch.height, h, 5*Time.deltaTime);
+    var lastHeight = ch.height; // crouch/stand up smoothly
     
+    ch.height = Mathf.Lerp(ch.height, h, 5*Time.deltaTime);
+    ch.radius = Mathf.Lerp(ch.radius, r, 5*Time.deltaTime);
     
     tr.position.y += (ch.height-lastHeight+temp)/2; // fix vertical position
     audio.volume=voice;
