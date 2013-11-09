e29e163ed5dbb72d0e6c90d7176c9057f0e1400a
abhishek17 <abhishek17aug1991@gmail.com>
2013-11-09 01:48:55 -0700

    Added Night to Day Change

---
+++ P3/Assets/DayNight.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+var colors : Color[];
+var colorr:int=0;
+private var startTime:float=0.0f;
+private var speed:float=0.000005f;
+function Start () {
+
+}
+
+function Update () {
+    RenderSettings.ambientLight = Color.Lerp (RenderSettings.ambientLight, Color.white, (Time.time - startTime) * speed);
+ 
+ 	if(RenderSettings.ambientLight ==Color.white)
+ 	Debug.Log("Ye Ye");
+    //this is just to test
+    //gameO.transform.position = Vector3.Lerp(gameO.transform.position, pos.enemyStart, (Time.time - startTime) * speed);
+ 
+}
