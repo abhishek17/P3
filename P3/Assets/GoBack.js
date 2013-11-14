ef78973f2482e325f541a40d81af33386e3d3f7a
abhishek17 <abhishek17aug1991@gmail.com>
2013-11-14 01:00:33 -0700

    Flashback

---
+++ P3/Assets/GoBack.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+private var timeE:int;
+private var timeS:int;
+
+private static var once=false;
+private var flash:GameObject;
+function Start () {
+timeS=0;
+timeE=100;
+//once=false;
+}
+
+function Update () {
+	if(timeS<timeE)
+	{
+	timeS++;
+	}
+	else
+	{
+	if(!once)
+	{
+	once=true;
+	flash=GameObject.Find("Flash");
+	Destroy (flash);
+	//Application.LoadLevelAdditive("FirstPerson");
+	}
+	}
+
+}
---
+++ P3/Assets/Movie.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+private static var once=false;
+private var player:GameObject;
+//var pitch : Crouch;
+function Start()
+{
+//once=false;
+}
+function OnTriggerEnter (other : Collider) {
+	
+		// Debug-draw all contact points and normals
+		if(other.name=="First Person Controller")
+		{
+		if(!once)
+		{
+		once=true;
+		player=GameObject.Find("Main Camera");
+		//player.audio.pitch=20.8f;
+		//pitch=player.GetComponent(Crouch);
+		player.audio.pitch=1.3f;
+		Debug.Log("play movie");
+		Application.LoadLevelAdditive("Flashback");
+		}
+		}
+		}
